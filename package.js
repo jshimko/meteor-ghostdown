b47ec178d662b59544a4e5b5233e48743176f4ed
Jeremy Shimko <jshimko@cloudlinemedia.co>
2014-09-01 19:14:05 -0400

    clean up package.js

--- package.js
+++ package.js
@@ -2,20 +2,22 @@ Package.describe({
      name: "jeremy:ghostdown",
      summary: "A split-pane Markdown editor by the fine folks at Ghost.org",
      version: '0.1.0',
-     git: 'https://github.com/jshimko/meteor-ghostdown'
+     git: 'https://github.com/jshimko/meteor-ghostdown.git'
 });
 
 Package.onUse(function (api) {
 
     api.versionsFrom("METEOR@0.9.0");
 
-    api.use('jquery', 'client');
+    api.use([
+        'jquery'
+        ], 'client');
 
     api.addFiles([
         'ghost-markdown-editor/ghostdown.css',
-        'ghost-markdown-editor/ghostdown.js',
+        'ghost-markdown-editor/ghostdown.js'
         ], 'client');
 
-    api.export('CodeMirror');
-
+    if (api.export)
+        api.export('CodeMirror');
 });
