61edcce58d5ed352962a1d14fe631c11203367d1
Jeremy Shimko <jeremy.shimko@gmail.com>
2015-04-28 16:29:40 -0400

    formatting to 2 spaces

--- package.js
+++ package.js
@@ -1,24 +1,25 @@
 Package.describe({
-     name: "jeremy:ghostdown",
-     summary: "A split-pane Markdown editor by the fine folks at Ghost.org",
-     version: '0.3.1',
-     git: 'https://github.com/jshimko/meteor-ghostdown.git'
+  name: "jeremy:ghostdown",
+  summary: "A split-pane Markdown editor by the fine folks at Ghost.org",
+  version: '0.4.0',
+  git: 'https://github.com/jshimko/meteor-ghostdown.git'
 });
 
-Package.onUse(function (api) {
+Package.onUse(function(api) {
 
-    api.versionsFrom("METEOR@0.9.1.1");
+  api.versionsFrom("METEOR@0.9.2");
 
-    api.use([
-        'jquery',
-        'templating'
-        ], 'client');
+  api.use([
+    'jquery',
+    'templating'
+  ], 'client');
 
-    api.addFiles([
-        'lib/client/vendor/codemirror.js',
-        'lib/client/vendor/showdown.js',
-        'lib/client/codemirror-markdown.js',
-        'lib/client/ghostdown.html',
-        'lib/client/ghostdown.js'
-        ], 'client');
+  api.addFiles([
+    'lib/client/vendor/codemirror.js',
+    'lib/client/vendor/showdown.js',
+    'lib/client/codemirror-markdown.js',
+    'lib/client/ghostdown.html',
+    'lib/client/ghostdown.js'
+  ], 'client');
+
 });
